b79ae4430cb515b1b578411c5ee2fbafa102977e
Zack P <zackjp87@gmail.com>
2026-02-11 15:44:34 -0500

    [Dashboard] Minor cleanup: extract dashboard views to composables, and strings to resources

--- app/src/main/java/com/zackjp/devicedx/feature/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/zackjp/devicedx/feature/dashboard/DashboardScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -17,9 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.zackjp.devicedx.R
 
 @Composable
 fun DashboardScreen(
@@ -48,48 +51,71 @@ fun DashboardScreen(
         modifier = modifier,
     ) {
         item {
-            Text("Device Diagnostics")
+            Text(stringResource(R.string.device_diagnostics))
         }
 
         item {
             Spacer(Modifier.height(16.dp))
-            Row {
-                Button(
-                    onClick = { viewModel.onStartScan() },
-                ) {
-                    Text("Get Wifi SSIDs")
-                }
-
-                Spacer(Modifier.width(16.dp))
-
-                Button(
-                    onClick = { viewModel.onMonitorLatency() },
-                ) {
-                    Text("Get Network Speeds")
-                }
-            }
+            DiagnosticButtonRow(
+                modifier = Modifier.fillMaxWidth(),
+                onStartLatencyMonitor = { viewModel.onMonitorLatency() },
+                onStartWifiScan = { viewModel.onStartScan() },
+            )
         }
 
         when (state.activeView) {
-            DashboardView.Unselected -> {
-                item {
-                    Text("Select your diagnostic")
-                }
-            }
-            DashboardView.Wifi -> {
-                items(state.wifiNames) { wifiName ->
-                    Spacer(Modifier.height(16.dp))
-                    Text(wifiName)
-                }
-            }
-            DashboardView.Latency -> {
-                item {
-                    Column(Modifier.fillMaxWidth()) {
-                        val latency = state.latencyMillis
-                        Text("Latency: ${latency}ms")
-                    }
-                }
-            }
+            DashboardView.Unselected -> unselectedDiagnostics()
+            DashboardView.Wifi -> wifiScanResults(state.wifiNames)
+            DashboardView.Latency -> latencyResult(state.latencyMillis)
+        }
+    }
+}
+
+@Composable
+private fun DiagnosticButtonRow(
+    modifier: Modifier = Modifier,
+    onStartLatencyMonitor: () -> Unit = {},
+    onStartWifiScan: () -> Unit = {},
+) {
+    Row(modifier) {
+        Button(
+            onClick = onStartWifiScan,
+        ) {
+            Text(stringResource(R.string.get_wifi_ssids))
+        }
+
+        Spacer(Modifier.width(16.dp))
+
+        Button(
+            onClick = onStartLatencyMonitor,
+        ) {
+            Text(stringResource(R.string.get_network_speeds))
+        }
+    }
+}
+
+private fun LazyListScope.unselectedDiagnostics() {
+    item {
+        Text(stringResource(R.string.select_your_diagnostic))
+    }
+}
+
+private fun LazyListScope.wifiScanResults(
+    wifiNames: List<String>,
+) {
+    items(wifiNames) { wifiName ->
+        Spacer(Modifier.height(16.dp))
+        Text(wifiName)
+    }
+}
+
+private fun LazyListScope.latencyResult(
+    latencyMillis: Long,
+    modifier: Modifier = Modifier,
+) {
+    item {
+        Column(modifier) {
+            Text(stringResource(R.string.latency_ms, latencyMillis))
         }
     }
 }
