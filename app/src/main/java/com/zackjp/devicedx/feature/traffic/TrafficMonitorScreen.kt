7732d10b33c8a94d18b25a848eaa6cf8595488a3
Zack P <zackjp87@gmail.com>
2026-02-26 16:31:22 -0500

    [Traffic] Use rounded corners on the traffic graph

--- app/src/main/java/com/zackjp/devicedx/feature/traffic/TrafficMonitorScreen.kt
+++ app/src/main/java/com/zackjp/devicedx/feature/traffic/TrafficMonitorScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
@@ -86,6 +87,7 @@ private fun TrafficMonitorScreen(
             trafficMetrics = state.trafficMetrics,
             rxLineColor = RxLineColor,
             modifier = Modifier
+                .clip(MaterialTheme.shapes.medium)
                 .background(Color.Black)
                 .fillMaxWidth()
                 .aspectRatio(ASPECT_RATIO_FLOAT),
