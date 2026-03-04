3f9d3eee826d9d67a14f6ca95db9b6f389e54c59
Zack P <zackjp87@gmail.com>
2026-03-04 18:08:17 -0500

    [App] Update theme to use neon blue/green for primary/secondary colors

--- app/src/main/java/com/zackjp/devicedx/ui/theme/Color.kt
+++ app/src/main/java/com/zackjp/devicedx/ui/theme/Color.kt
@@ -5,6 +5,5 @@ import androidx.compose.ui.graphics.Color
 val DarkSlate = Color(0xFF222829)
 val OffWhite = Color(0xFFF1F1F1)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val NeonBlue = Color(0xFF6F9FC6)
+val NeonGreen = Color(0xFF8FC2A6)
--- app/src/main/java/com/zackjp/devicedx/ui/theme/Theme.kt
+++ app/src/main/java/com/zackjp/devicedx/ui/theme/Theme.kt
@@ -3,13 +3,12 @@ package com.zackjp.devicedx.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-    outlineVariant = Color.Magenta,
+    primary = NeonBlue,
+    onPrimary = DarkSlate,
+    secondary = NeonGreen,
+    outlineVariant = OffWhite,
     onBackground = OffWhite,
     onSurface = OffWhite,
     onSurfaceVariant = OffWhite,
