1b723da3f815fc4d598959a0d2f4b13f95444fa0
Zack P <zackjp87@gmail.com>
2026-03-19 16:00:13 -0400

    [Traffic] Mark TrafficScreenState as @Immutable
    - no current performance issues, but might as well

--- app/src/main/java/com/zackjp/devicedx/feature/traffic/TrafficScreenState.kt
+++ app/src/main/java/com/zackjp/devicedx/feature/traffic/TrafficScreenState.kt
@@ -1,8 +1,11 @@
 package com.zackjp.devicedx.feature.traffic
 
+import androidx.compose.runtime.Immutable
 import com.zackjp.devicedx.model.TrafficMetric
 import com.zackjp.devicedx.model.TrafficSession
 
+
+@Immutable
 data class TrafficScreenState(
     val graphData: List<TrafficMetric>,
     val isMonitorActive: Boolean,
